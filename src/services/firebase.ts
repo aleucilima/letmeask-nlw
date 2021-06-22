b9d89178c8a380221461d43dd731d0c450cf4ac3
aleucilima <aleuci.lima@gmail.com>
2021-06-22 16:27:56 -0300

    Letmeask

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/auth'
 import 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSbdZEsZE1jIMiYIY1qb_GFvTOel9JJ3w",
-  authDomain: "letmeask-af49d.firebaseapp.com",
-  databaseURL: "https://letmeask-af49d-default-rtdb.firebaseio.com",
-  projectId: "letmeask-af49d",
-  storageBucket: "letmeask-af49d.appspot.com",
-  messagingSenderId: "469745025393",
-  appId: "1:469745025393:web:84a9abf4422104fa0af6d0"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig)
