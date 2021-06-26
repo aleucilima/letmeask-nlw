323d3c7446836f5c469ebde15b36f5b3459302c9
aleucilima <aleuci.lima@gmail.com>
2021-06-26 20:40:20 -0300

    letmeask

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,33 +1,17 @@
 import { useContext } from 'react'
-import { useHistory } from 'react-router-dom'
-
-import { auth, firebase } from '../services/firebase'
+import { Link } from 'react-router-dom'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
 import { Button } from '../components/Button'
-
 import { TestContext } from '../App'
-
 
 import '../styles/auth.scss'
 
-export function Home() {
-  const history = useHistory()
+export function NewRoom() {
   const { value, setValue } = useContext(TestContext)
-
-  function handleCreateRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider()
-
-    auth.signInWithPopup(provider).then(result => {
-
-      history.push('/rooms/new')
-    })
-
-  }
-
   return (
     <div id="page-auth">
       <aside>
@@ -38,20 +22,19 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button onClick={handleCreateRoom} className="create-room">
-            <img src={googleIconImg} alt="Logo do Google" />
-            Crie sua sala com o Google
-          </button>
-          <div className="separator">ou entre em uma sala</div>
+          <h2>Criar uma nova sala</h2>
           <form>
             <input 
               type="text"
-              placeholder="Digite o código da sala" 
+              placeholder="Nome da sala" 
             />
             <Button type="submit">
-              Entrar na sala
+              Criar sala
             </Button>
           </form>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+          </p>
         </div>
       </main>
     </div>
