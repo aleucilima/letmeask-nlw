b7bd027f421d10715bdf14560b7aa0d36f223389
aleucilima <aleuci.lima@gmail.com>
2021-06-26 21:39:03 -0300

    letmeask

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react'
 import { useHistory } from 'react-router-dom'
 
-import { auth, firebase } from '../services/firebase'
+import { AuthContext } from '../contexts/AuthContext'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
@@ -13,8 +13,12 @@ import '../styles/auth.scss'
 
 export function Home() {
   const history = useHistory()
+  const { user, signInWithGoogle } = useContext(AuthContext)
 
   function handleCreateRoom() {
+    if(!user) {
+      signInWithGoogle()
+    }
     history.push('/rooms/new')
   }
 
